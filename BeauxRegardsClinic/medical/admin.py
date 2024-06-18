4d06778c70b8a10435cbc500dbad45bd03676473
mvasam <66389738+mvasam@users.noreply.github.com>
2024-06-19 03:00:51 +0530

    Medical staff and Patient

--- BeauxRegardsClinic/medical/admin.py
+++ BeauxRegardsClinic/medical/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import MedicalStaff, Patient
 
-# Register your models here.
+@admin.register(MedicalStaff)
+class MedicalStaffAdmin(admin.ModelAdmin):
+    list_display = ('staff_id', 'first_name', 'last_name', 'role', 'department', 'email')
+    search_fields = ('first_name', 'last_name', 'email', 'department')
+    list_filter = ('role', 'department', 'status')
+    ordering = ('staff_id',)
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('patient_id', 'first_name', 'last_name', 'email', 'assigned_doctor')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('gender', 'assigned_doctor')
+    ordering = ('patient_id',)
